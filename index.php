0254988a18628fed21dc4e9202e5231ed2703b31
dembasy <dembasy1227@gmail.com>
2018-08-20 10:45:05 +0200

    changement isset empty

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8" />
-    <title>exercice 3</title>
-  </head>
-  <body>
-      <p>
-      <?php
-      //grâce a la condition et a isset on va pouvoir recuperer le startDate et le endDate pour l'afficher
-        if (isset($_GET['startDate']) && isset($_GET['endDate'])) {
-        echo $_GET['startDate'] . ' ' . $_GET['endDate'];
-      }
-      ?>
-      </p>
-  </body
+    <head>
+        <meta charset="utf-8" />
+        <title>exercice 3</title>
+    </head>
+    <body>
+        <p>
+            <?php
+//Condition pour vérifier l'existence des paramètres
+            if (!empty($_GET['startDate']) && !empty($_GET['endDate'])) {
+                //Affichage des paramètres
+                echo $_GET['startDate'] . ' - ' . $_GET['endDate'];
+            } else {
+                echo 'Il manque un paramètre';
+            }
+            ?>
+        </p>
+    </body
 </html>
